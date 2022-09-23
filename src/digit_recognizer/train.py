42625a4eed334be3273e44c8fd87bc6e3a94c40d
CodeXploreRePo <codexplore.channel@gmail.com>
2022-09-23 18:40:00 +0800

    train implement

---
+++ src/digit_recognizer/train.py
@@ -0,0 +1,46 @@
+"""This module is to train the models
+"""
+
+import argparse
+
+
+def get_argument_parser():
+    """
+    Argument parser which returns the options which the user inputted.
+
+    Args:
+        None
+
+    Returns:
+        argparse.ArgumentParser().parse_args()
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-e",
+        "--epochs",
+        help="How many epochs you need to run (default: 10)",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "-bs",
+        "--batch_size",
+        help="The number of images in a batch (default: 64)",
+        type=int,
+        default=64,
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def train():
+    """This is for training models"""
+    # load all arguments
+    args = get_argument_parser()
+    print(args.epochs, args.batch_size)
+
+
+if __name__ == "__main__":
+    train()
