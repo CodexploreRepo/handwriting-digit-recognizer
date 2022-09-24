633d2406febe92d1a9b1270c199243d628af408a
benjaminlq <benjamin.lequan@gmail.com>
2022-09-24 18:28:51 +0800

    test

---
+++ src/digit_recognizer/tests/test_models.py
@@ -0,0 +1,14 @@
+"""This module is to test
+"""
+
+
+def hello(a):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a
