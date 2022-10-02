d2d0b990bce9bf7baf2936bea8c27c0906d86113
benjaminlq <benjamin.lequan@gmail.com>
2022-10-02 19:37:13 +0800

    Update model test for greyscale & rbg

--- src/digit_recognizer/tests/test_models.py
+++ src/digit_recognizer/tests/test_models.py
@@ -3,12 +3,17 @@
 import torch
 
 from digit_recognizer.models.conv_net import BasicConvNet
+from digit_recognizer.models.pretrain import *
 
 
 def test_model():
     """Test that model outputs correct size"""
-    sample = torch.randn(3, 1, 28, 28)
-    model = BasicConvNet()
-    out_sample = model(sample)
+    test_dict = {(1, 28, 28): [BasicConvNet()], (3, 28, 28): [PretrainedResNet50()]}
 
-    assert out_sample.shape == torch.Size([3, 10]), "Output Size mismatch"
+    for img_size in test_dict.keys():
+        models = test_dict[img_size]
+        c, h, w = img_size
+        sample = torch.randn(3, c, h, w)
+        for model in models:
+            out_sample = model(sample)
+            assert out_sample.shape == torch.Size([3, 10]), "Output Size mismatch"
