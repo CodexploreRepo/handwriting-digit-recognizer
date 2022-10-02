e36d9ee339ebd61db2766d5a7869d861ab560404
benjaminlq <benjamin.lequan@gmail.com>
2022-10-02 19:45:39 +0800

    Setup MODEL_PARAMS dict

--- src/digit_recognizer/config.py
+++ src/digit_recognizer/config.py
@@ -4,6 +4,9 @@ This is the config file of Digit Recognizer
 import os
 import pathlib
 
+from digit_recognizer.models.conv_net import BasicConvNet
+from digit_recognizer.models.pretrain import PretrainedResNet50
+
 SEED = 42
 
 # --- PATH ----
@@ -14,3 +17,14 @@ MODEL_PATH = MAIN_PATH / "model_chkpt"
 RESULT_PATH = MAIN_PATH / "docs" / "submissions"
 # --- CPU ---
 NUM_WORKERS = os.cpu_count()
+# --- MODEL ---
+MODEL_PARAMS = {
+    "basic_conv_net": {
+        "model": BasicConvNet,
+        "rbg": False,
+    },
+    "pretrained_resnet50": {
+        "model": PretrainedResNet50,
+        "rbg": True,
+    },
+}
