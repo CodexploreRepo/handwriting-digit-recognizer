3db7d9e5a4889c5c4c7e86a0342dfb3026c38d35
benjaminlq <benjamin.lequan@gmail.com>
2022-10-02 20:26:28 +0800

    Update script to test multiple models

--- src/digit_recognizer/tests/test_models.py
+++ src/digit_recognizer/tests/test_models.py
@@ -1,19 +1,22 @@
-"""This module is to test Model
+"""This module is to test Models
 """
 import torch
 
-from digit_recognizer.models.conv_net import BasicConvNet
-from digit_recognizer.models.pretrain import *
+from digit_recognizer.config import MODEL_PARAMS
 
 
 def test_model():
-    """Test that model outputs correct size"""
-    test_dict = {(1, 28, 28): [BasicConvNet()], (3, 28, 28): [PretrainedResNet50()]}
+    """Test all models for correct output size.
+    Register the model settings in config.py"""
 
-    for img_size in test_dict.keys():
-        models = test_dict[img_size]
-        c, h, w = img_size
-        sample = torch.randn(3, c, h, w)
-        for model in models:
-            out_sample = model(sample)
-            assert out_sample.shape == torch.Size([3, 10]), "Output Size mismatch"
+    for model_name in MODEL_PARAMS:
+        model = MODEL_PARAMS[model_name]["model"]()
+        if MODEL_PARAMS[model_name]["rbg"]:
+            sample = torch.randn(3, 3, 28, 28)
+        else:
+            sample = torch.randn(3, 1, 28, 28)
+
+        out_sample = model(sample)
+        assert out_sample.shape == torch.Size(
+            [3, 10]
+        ), f"Output Size mismatch for {model_name}"
