9cce690045db2667b5fe596c3dfa8e8bc4074001
CodeXploreRePo <codexplore.channel@gmail.com>
2022-10-04 21:30:51 +0800

    add upload heloo in router

--- backend/app/routers/hello.py
+++ backend/app/routers/hello.py
@@ -20,3 +20,28 @@ def add(body: AddPayload):
 @router.get("/env")
 def get_env(settings: Settings = Depends(get_settings)):
     return {"env": settings.environment}
+
+
+from fastapi import File, UploadFile
+
+
+@router.post("/upload")
+def upload(file: UploadFile = File(...)):
+    """Upload
+
+    Args:
+        file (UploadFile, optional): _description_. Defaults to File(...).
+
+    Returns:
+        _type_: _description_
+    """
+    try:
+        contents = file.file.read()
+        with open(file.filename, "wb") as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
