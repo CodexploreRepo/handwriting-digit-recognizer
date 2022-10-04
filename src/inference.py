09beb5029008dfc386c1c6b7c11f69207034f109
CodeXploreRePo <codexplore.channel@gmail.com>
2022-10-04 13:31:39 +0800

    add a inference

--- src/inference.py
+++ src/inference.py
@@ -86,3 +86,29 @@ if __name__ == "__main__":
     args = get_argument_parser()
     version = args.version
     inference_kaggle(version)
+
+    #     # sample execution (requires torchvision)
+    # from PIL import Image
+    # from torchvision import transforms
+    # input_image = Image.open(filename)
+    # preprocess = transforms.Compose([
+    #     transforms.Resize(299),
+    #     transforms.CenterCrop(299),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    # ])
+    # input_tensor = preprocess(input_image)
+    # input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+    # # move the input and model to GPU for speed if available
+    # if torch.cuda.is_available():
+    #     input_batch = input_batch.to('cuda')
+    #     model.to('cuda')
+
+    # with torch.no_grad():
+    #     output = model(input_batch)
+    # # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
+    # print(output[0])
+    # # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+    # probabilities = torch.nn.functional.softmax(output[0], dim=0)
+    # print(probabilities)
